9ca9da89e30a24551dc0fbba1c99a771541dd1d0
yaua305 <95682002+yaua305@users.noreply.github.com>
2024-05-11 18:26:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hi em Cún nhá',
+    introDesc: `Cho anh hỏi em 1 câu nhớ trả lời thật lòng và điền đầy đủ thông tin vào`,
+    btnIntro: 'Không thật lòng làm chó ',
+    title: 'Em là 1 con khỉ hay 1 củ carot dễ thương á',
+    desc: ' Chọn 1 trong 2 đi em ',
+    btnYes: 'Em là 1 con khỉ á ',
+    btnNo: 'Em là củ carot dễ thương nè',
+    question: 'Nói cho lý do vì sao em chọn là 1 con khỉ đi ',
+    btnReply: 'Gửi cho anh Dũng dễ thương đẹp trai',
+    reply: 'Tại vì em hay nhảy nhót chạy nhảy lung tung á , với tại anh Dũng dễ thương quá nữa , thích anh Dũng quá trời ',
+    mess: 'Anh biết em thích anh mà .Cố gắng bớt nhảy nhót lại . Em mãi là 1 bé khỉ dễ thương ',
+    messDesc: 'Sẵn sàng xem nhảy ếch chưa',
+    btnAccept: 'Sẵn sàng',
+    messLink: 'https://yaua305.github.io/concun/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
